b9fc845a7a0d246877b8236a5b85095f8550708d
Charles Chen <charleschenster@gmail.com>
2011-02-24 12:39:54 -0800

    Old Code, still stable

--- Classes/ProgViewController.m
+++ Classes/ProgViewController.m
@@ -43,35 +43,35 @@
 	
 	if ([currentView objectForKey:@"image"] != nil) {
 		imageFilePath = [documentsDirectory stringByAppendingPathComponent:[currentView objectForKey:@"image"]];
-		
-		/*UIColor *background = [[UIColor alloc] initWithPatternImage:
-		 [[UIImage imageWithContentsOfFile:imageFilePath] stretchableImageWithLeftCapWidth:0.0 topCapHeight:0.0]];
-		 */
+
 		UIImage *bgimage = [[UIImage imageWithContentsOfFile:imageFilePath] stretchableImageWithLeftCapWidth:0.0 topCapHeight:0.0];
-		//[bgimage drawInRect:rect];
-		
+
 		UIImageView *bgimageview = [[UIImageView alloc] initWithImage:bgimage];
 		[bgimageview setFrame:rect];
 		
 		[self.view addSubview:bgimageview];
+		[bgimage release];
+		[bgimageview release];
 	}
 	
-	NSLog(@"Loading View!");
-	NSLog(@"View dimensions should be %d, %d, %d, %d", 
+	NSLog(@"Loading View");
+	/*NSLog(@"View dimensions should be %d, %d, %d, %d", 
 		  [[currentView objectForKey:@"x"] intValue], 
 		  [[currentView objectForKey:@"y"] intValue],
 		  [[currentView objectForKey:@"xsize"] intValue], 
-		  [[currentView objectForKey:@"ysize"] intValue]);
-
-	NSLog(@"Ready!");
+		  [[currentView objectForKey:@"ysize"] intValue]);*/
 	
 	NSLog(@"Generating buttons");
 	
+	if (arrayButton != nil) {
+		[arrayButton release];
+	}
 	arrayButton = [NSMutableArray new];
+	if (arrayButtonDict != nil) {
+		[arrayButtonDict release];
+	}
 	arrayButtonDict = [NSMutableArray new];
-	NSArray *buttonKeys = [[self.dictionary objectForKey:@"button"] allKeys];
-	NSDictionary *currentButton;
-	
+		
 	NSArray *subviews = [[self.dictionary objectForKey:@"view"] objectForKey:@"subview"];
 	
 	for (int i = 0; i < subviews.count; i++) {
@@ -79,7 +79,8 @@
 		if ([[[subviews objectAtIndex:i] objectForKey:@"type"] isEqualToString:@"button"]) {
 			NSDictionary *currentButton = [subviews objectAtIndex:i];
 			[arrayButton addObject:[UIButton buttonWithType:UIButtonTypeCustom]];
-			NSLog(@"Button has x location %d", [[currentButton objectForKey:@"x"] intValue]);
+			
+			//NSLog(@"Button has x location %d", [[currentButton objectForKey:@"x"] intValue]);
 			[[arrayButton objectAtIndex:arrayButton.count -1] setFrame:CGRectMake([[currentButton objectForKey:@"x"] intValue], [[currentButton objectForKey:@"y"] intValue], [[currentButton objectForKey:@"xsize"] intValue], [[currentButton objectForKey:@"ysize"] intValue])];
 			
 			if ([currentButton objectForKey:@"title"] != nil) {
@@ -99,13 +100,12 @@
 			
 		}
 		if ([[[subviews objectAtIndex:i] objectForKey:@"type"] isEqualToString:@"text"]) {
-			NSLog(@"%@", [subviews objectAtIndex:i]);
+			//NSLog(@"%@", [subviews objectAtIndex:i]);
 			NSDictionary *currentText = [subviews objectAtIndex:i];
 			UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake([[currentText objectForKey:@"x"] intValue], 
 																	   [[currentText objectForKey:@"y"] intValue], 
 																	   [[currentText objectForKey:@"xsize"] intValue], 
 																	   [[currentText objectForKey:@"ysize"] intValue])];
-			//NSLog(@"Test1");
 			label.text = [currentText objectForKey:@"txt"];
 			
 			if ([currentText objectForKey:@"r"] != nil) {
@@ -125,63 +125,6 @@
 			[self.view addSubview:label];	
 		}
 	}
-	
-	/*
-	for (int i = 0; i < buttonKeys.count; i++) {
-		NSLog(@"Button %@", [buttonKeys objectAtIndex:i]);
-		currentButton = [[self.dictionary objectForKey:@"button"] objectForKey:[buttonKeys objectAtIndex:i]];
-		[arrayButton addObject:[UIButton buttonWithType:UIButtonTypeCustom]];
-		NSLog(@"Button %@ has x location %d", [buttonKeys objectAtIndex:i], [[currentButton objectForKey:@"x"] intValue]);
-		//[array objectAtIndex:i].frame = CGRectMake([[currentButton objectForKey:@"x"] intValue], [[currentButton objectForKey:@"y"] intValue], [[currentButton objectForKey:@"xsize"] intValue], [[currentButton objectForKey:@"ysize"] intValue]);
-		
-		
-		[[arrayButton objectAtIndex:i] setFrame:CGRectMake([[currentButton objectForKey:@"x"] intValue], [[currentButton objectForKey:@"y"] intValue], [[currentButton objectForKey:@"xsize"] intValue], [[currentButton objectForKey:@"ysize"] intValue])];
-		
-		if ([currentButton objectForKey:@"title"] != nil) {
-			[[arrayButton objectAtIndex:i] setTitle:[currentButton objectForKey:@"title"] forState:UIControlStateNormal];
-		}
-		
-		imageFilePath = [documentsDirectory stringByAppendingPathComponent:[currentButton objectForKey:@"image"]];
-		if([fileManager fileExistsAtPath:imageFilePath])
-		{
-			[[arrayButton objectAtIndex:i] setBackgroundImage:[[UIImage imageWithContentsOfFile:imageFilePath] stretchableImageWithLeftCapWidth:0.0 topCapHeight:0.0] forState:UIControlStateNormal];
-		}
-		
-		[[arrayButton objectAtIndex:i] addTarget:self action:NSSelectorFromString([currentButton objectForKey:@"imageAction"]) forControlEvents:UIControlEventTouchUpInside];
-		[[arrayButton objectAtIndex:i] setTag:i];
-		[arrayButtonDict addObject:currentButton];
-		[self.view addSubview:[arrayButton objectAtIndex:i]];										
-	}*/
-	/*
-	NSLog(@"Generating text");
-	NSArray *textKeys = [[self.dictionary objectForKey:@"text"] allKeys];
-	NSDictionary *currentText;
-	UILabel *label;
-	for (int i = 0; i < textKeys.count; i++) {
-		NSLog(@"Text %@", [textKeys objectAtIndex:i]);
-		currentText = [[self.dictionary objectForKey:@"text"] objectForKey:[textKeys objectAtIndex:i]];
-		label = [[UILabel alloc] initWithFrame:CGRectMake([[currentText objectForKey:@"x"] intValue], 
-														  [[currentText objectForKey:@"y"] intValue], 
-														  [[currentText objectForKey:@"xsize"] intValue], 
-														  [[currentText objectForKey:@"ysize"] intValue])];
-		label.text = [currentText objectForKey:@"txt"];
-		
-		if ([currentText objectForKey:@"r"] != nil) {
-			label.textColor = [[UIColor alloc] initWithRed:[[currentText objectForKey:@"r"] floatValue] 
-													 green:[[currentText objectForKey:@"g"] floatValue] 
-													  blue:[[currentText objectForKey:@"b"] floatValue] 
-													 alpha:[[currentText objectForKey:@"alpha"] floatValue]];
-			
-		}
-		
-		if ([currentText objectForKey:@"font"] != nil) {
-			label.font = [UIFont fontWithName:[currentText objectForKey:@"font"] 
-										 size:[[currentText objectForKey:@"fontsize"] floatValue]];
-		}
-		
-		label.backgroundColor = [UIColor clearColor];
-		[self.view addSubview:label];		
-	}*/
 }
 
 - (void) reloadView {
@@ -275,7 +218,6 @@
 - (void) buttonPressed4: (id) sender {
 	NSLog(@"Called buttonPressed4 from %d", [sender tag]);
 	[self.view removeFromSuperview];
-	//[self.progViewController loadView];
 }
 
 
@@ -318,27 +260,49 @@
 		self.ready = NO;
 		reload = NO;
 		self.dictionary = nil;
+		responseString = nil;
 		responseDataHashes = nil;
+		arrayButton = nil;
+		arrayButtonDict = nil;
+		arrayASI = nil;
+		arrayASIfinished = nil;
 		urlstringjson = @"http://pandamoniumpanda.appspot.com/jsonbanner";
 		urlstringhash = @"http://pandamoniumpanda.appspot.com/jsonbannerhash";
 		
-		NSURL *url = [NSURL URLWithString:urlstringjson];
-		requestjson = [ASIHTTPRequest requestWithURL:url];
-		[requestjson setDelegate:self];
-		[requestjson startAsynchronous];
-		[requestjson setNumberOfTimesToRetryOnTimeout:2];
+		NSFileManager *fileManager = [NSFileManager defaultManager];
+		NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+		NSString *documentsDirectory = [paths objectAtIndex:0];	
 		
-		NSURL *urlhash = [NSURL URLWithString:urlstringhash];
-		requesthash = [ASIHTTPRequest requestWithURL:urlhash];
-		[requesthash setDelegate:self];
-		[requesthash startAsynchronous];
-		[requesthash setNumberOfTimesToRetryOnTimeout:2];
+		NSString *hashPath = [documentsDirectory stringByAppendingPathComponent:@"pushhash.data"];
+		
+		if ([fileManager fileExistsAtPath:hashPath]) {
+			self.ready = YES;
+			NSURL *urlhash = [NSURL URLWithString:urlstringhash];
+			responseDataHashes = [[NSData alloc] initWithData:[fileManager contentsAtPath:hashPath]];
+			requesthash = [ASIHTTPRequest requestWithURL:urlhash];
+			[requesthash setDelegate:self];
+			[requesthash startAsynchronous];
+			[requesthash setNumberOfTimesToRetryOnTimeout:2];
+		} else {
+			NSURL *url = [NSURL URLWithString:urlstringjson];
+			requestjson = [ASIHTTPRequest requestWithURL:url];
+			[requestjson setDelegate:self];
+			[requestjson startAsynchronous];
+			[requestjson setNumberOfTimesToRetryOnTimeout:2];
+			
+			NSURL *urlhash = [NSURL URLWithString:urlstringhash];
+			requesthash = [ASIHTTPRequest requestWithURL:urlhash];
+			[requesthash setDelegate:self];
+			[requesthash startAsynchronous];
+			[requesthash setNumberOfTimesToRetryOnTimeout:2];
+		}
     }
     return self;
 }
 
 - (void)requestFinished:(ASIHTTPRequest *)request
 {
+	NSLog(@"Request finished, identifying request");
 	// Use when fetching binary data
 	//NSData *responseData = [request responseData];
 	if (request == requestjson) {
@@ -350,6 +314,7 @@
 			NSString *responseCompare = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
 			
 			if ([responseString isEqualToString:responseCompare]) {
+				[responseCompare release];
 				NSLog(@"Same JSON no update needed");
 				return;
 			} else {
@@ -359,45 +324,27 @@
 				self.dictionary = nil;
 				[responseString release];
 				responseString = responseCompare;
-				//[arrayASI makeObjectsPerformSelector:@selector(release)];
 				[arrayASI release];
-				//[arrayASIfinished makeObjectsPerformSelector:@selector(release)];
+				arrayASI = nil;
 				[arrayASIfinished release];
+				arrayASIfinished = nil;
 			}
 		} else {
 			responseString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
 		}
 		
-		NSLog(responseString);
-		
 		self.dictionary = [responseString JSONValue];
-		NSLog(@"Dictionary value for \"image\" 0 is \"%@\"", [[[self.dictionary objectForKey:@"button"] objectForKey:@"0"] objectForKey:@"image"]);	
-		NSLog(@"Dictionary value for \"imageAction\" 0 is \"%@\"", [[[self.dictionary objectForKey:@"button"] objectForKey:@"0"] objectForKey:@"imageAction"]);	
-		NSLog(@"Dictionary value for \"x\" 0 is \"%d\"", [[[[self.dictionary objectForKey:@"button"] objectForKey:@"0"] objectForKey:@"x"] intValue]);
-		NSLog(@"Dictionary value for \"imageDict\" \"test\" is \"%d\"", [[[self.dictionary objectForKey:@"imageDict"] objectForKey:@"test"] intValue]);	
-		
-		NSArray *temp = [self.dictionary allKeys];
-		
-		NSEnumerator *enumerator = [temp objectEnumerator];
-		id anObject;
-		
-		NSLog(@"Dictionary has %d keys", temp.count);
-		NSLog(@"Dictionary has %d buttons", [[self.dictionary objectForKey:@"button"] allKeys].count);
-		
-		NSLog(@"Dictionary keys include:");
-		while (anObject = [enumerator nextObject]) {
-			NSLog(@"key: \"%@\"", anObject);
-		}
 		
 		// Get path to settings file.
 		NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
 		NSString *documentsDirectory = [paths objectAtIndex:0];
-		NSString *imageFilePath;
 		
 		// Get pointer to file manager.
 		NSFileManager *fileManager = [NSFileManager defaultManager];
 		
+		[arrayASI release];
 		arrayASI = [NSMutableArray new];
+		[arrayASIfinished release];
 		arrayASIfinished = [NSMutableArray new];
 		
 		NSArray *subviews = [[self.dictionary objectForKey:@"view"] objectForKey:@"subview"];
@@ -420,40 +367,14 @@
 					NSLog(@"Button image %@ found do not download", [currentButton objectForKey:@"image"]);
 					NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
 					ASIHTTPRequest *requestbutton = [ASIHTTPRequest requestWithURL:url];
+					/*ASIHTTPRequest *requestbutton = [ASIHTTPRequest requestWithURL:url];
 					[requestbutton setDelegate:self];
-					[requestbutton startAsynchronous];				
+					[requestbutton startAsynchronous];*/				
 					[arrayASI addObject:requestbutton];
 					[arrayASIfinished addObject:@"1"];
 				}
 			}
 		}
-		
-		/*
-		NSArray *buttonKeys = [[self.dictionary objectForKey:@"button"] allKeys];
-		NSDictionary *currentButton;
-		NSString *buttonFilePath;
-		for (int i = 0; i < buttonKeys.count; i++) {
-			currentButton = [[self.dictionary objectForKey:@"button"] objectForKey:[buttonKeys objectAtIndex:i]];
-			buttonFilePath = [documentsDirectory stringByAppendingPathComponent:[currentButton objectForKey:@"image"]];
-			if (![fileManager fileExistsAtPath:buttonFilePath]) {
-				NSLog(@"Button %@ image %@ not found commencing download", [buttonKeys objectAtIndex:i], [currentButton objectForKey:@"image"]);
-				
-				NSURL *url = [NSURL URLWithString:[currentButton objectForKey:@"imageurl"]];
-				ASIHTTPRequest *requestbutton = [ASIHTTPRequest requestWithURL:url];
-				[requestbutton setDelegate:self];
-				[requestbutton startAsynchronous];
-				[arrayASI addObject:requestbutton];
-				[arrayASIfinished addObject:[currentButton objectForKey:@"image"]];
-			} else {
-				NSLog(@"Button %@ image %@ found do not download", [buttonKeys objectAtIndex:i], [currentButton objectForKey:@"image"]);
-				NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
-				ASIHTTPRequest *requestbutton = [ASIHTTPRequest requestWithURL:url];
-				[requestbutton setDelegate:self];
-				[requestbutton startAsynchronous];				
-				[arrayASI addObject:requestbutton];
-				[arrayASIfinished addObject:@"1"];
-			}			
-		}*/
 		
 		NSDictionary *currentView = [self.dictionary objectForKey:@"view"];	
 		NSString *buttonFilePath;
@@ -473,8 +394,9 @@
 				NSLog(@"BG image %@ found do not download", [currentView objectForKey:@"image"]);
 				NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
 				ASIHTTPRequest *requestbutton = [ASIHTTPRequest requestWithURL:url];
+				/*ASIHTTPRequest *requestbutton = [ASIHTTPRequest requestWithURL:url];
 				[requestbutton setDelegate:self];
-				[requestbutton startAsynchronous];				
+				[requestbutton startAsynchronous];*/				
 				[arrayASI addObject:requestbutton];
 				[arrayASIfinished addObject:@"1"];
 			}
@@ -488,18 +410,27 @@
 	}
 	
 	if (request == requesthash) {
+		NSLog(@"Received hash");
 		NSData *data = [request responseData];
+		
 		if (self.ready == NO) {
-			NSLog(@"Received hash");
+			NSLog(@"Received old hash");
+			if (responseDataHashes != nil) {
+				[responseDataHashes release];
+			}
 			responseDataHashes = [[NSData alloc] initWithData:data];
 			
-			//responseStringHash = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+			NSFileManager *fileManager = [NSFileManager defaultManager];
+			NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+			NSString *documentsDirectory = [paths objectAtIndex:0];	
 			
-			//NSLog(@"%@",responseStringHash);
+			NSString *hashPath = [documentsDirectory stringByAppendingPathComponent:@"pushhash.data"];
+			
+			[fileManager createFileAtPath:hashPath contents:responseDataHashes attributes:nil];
+			
 			[self check_ready];
 		} else {
 			NSLog(@"Received new hash, comparing");
-			//NSString *responseCompare = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
 			
 			if ([responseDataHashes isEqualToData:data]) {
 				NSLog(@"Same signature no update needed");
@@ -509,51 +440,79 @@
 				self.ready = NO;
 				reload = YES;
 				self.dictionary = nil;
-				//[responseDataHash release];
+				[responseDataHashes release];
 				responseDataHashes = [[NSData alloc] initWithData:data];
+				
+				NSFileManager *fileManager = [NSFileManager defaultManager];
+				NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+				NSString *documentsDirectory = [paths objectAtIndex:0];	
+				
+				NSString *hashPath = [documentsDirectory stringByAppendingPathComponent:@"pushhash.data"];
+				
+				[fileManager createFileAtPath:hashPath contents:responseDataHashes attributes:nil];
+				
 				[arrayASI release];
+				arrayASI = nil;
 				[arrayASIfinished release];
-				//[self.view release];
+				arrayASIfinished = nil;
 				NSLog(@"Redownloading json to make sure up to date");
 				NSURL *url = [NSURL URLWithString:urlstringjson];
 				requestjson = [ASIHTTPRequest requestWithURL:url];
 				[requestjson setDelegate:self];
 				[requestjson startAsynchronous];
+				NSLog(@"Request dispatched");
 			}
 
 		}
-		
-		
 		return;		
 	}
-	// If request is not a requestjson then it is an image request
-	for (int i = 0; i < arrayASI.count; i++) {
-		if (request == [arrayASI objectAtIndex:i] && [arrayASIfinished objectAtIndex:i] != @"1") {
-			NSLog(@"Request for %d completed", i);
-			
-			NSData *responseData = [request responseData];
-			
-			NSFileManager *fileManager = [NSFileManager defaultManager];
-			NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-			NSString *documentsDirectory = [paths objectAtIndex:0];	
-			
-			NSString *imagePath = [documentsDirectory stringByAppendingPathComponent:[arrayASIfinished objectAtIndex:i]];
-			
-			[fileManager createFileAtPath:imagePath contents:responseData attributes:nil];
-			
-			[arrayASIfinished replaceObjectAtIndex:i withObject:@"1"];
-			
-			NSLog(@"Saved image to %@", imagePath);
-			
-			[self check_ready];
-			
-			return;
-			
+	
+	if (arrayASI != nil) {
+		// If request is not a requestjson then it is an image request
+		for (int i = 0; i < arrayASI.count; i++) {
+			if (request == [arrayASI objectAtIndex:i] && [arrayASIfinished objectAtIndex:i] != @"1") {
+				NSLog(@"Request for %d completed", i);
+				
+				NSData *responseData = [request responseData];
+				
+				NSFileManager *fileManager = [NSFileManager defaultManager];
+				NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+				NSString *documentsDirectory = [paths objectAtIndex:0];	
+				
+				NSString *imagePath = [documentsDirectory stringByAppendingPathComponent:[arrayASIfinished objectAtIndex:i]];
+				
+				[fileManager createFileAtPath:imagePath contents:responseData attributes:nil];
+				
+				[arrayASIfinished replaceObjectAtIndex:i withObject:@"1"];
+				
+				NSLog(@"Saved image to %@", imagePath);
+				
+				[self check_ready];
+				
+				return;
+				
+			}
 		}
 	}
 	
+}
+
+- (void)requestFailed:(ASIHTTPRequest *)request
+{
+	NSError *error = [request error];
+	NSLog(@"RESPONSE FAIL");
+	if (request == requestjson) {
+		NSLog(@"Could not pull the json");
+		return;
+	}
+	
+	if (request == requesthash) {
+		NSLog(@"Could not pull the hash");
+		return;
+	}
 	
 }
+
 
 - (void)check_ready {
 	if (self.ready == YES) {
